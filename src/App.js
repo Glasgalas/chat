21365cfe948f9ae11211f173fd5d033b2bfa437e
Glasgalas <alex.starichenko@icloud.com>
2022-02-23 18:06:50 +0200

    build

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Dialog from "./components/Dialog";
+import Sender from "./components/Sender";
+
+import { AppContext } from "./context";
+import { getTheme, setTheme } from "./helpers/theme";
+
+function App() {
+  const [message, addMessage] = useState(null);
+  const [theme, toggleTheme] = useState(getTheme());
+
+  useEffect(() => {
+    setTheme(theme);
+  }, [theme]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      <div className="container">
+        <Header />
+        <Dialog newMessage={message} />
+        <Sender onAddMessage={addMessage} />
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
